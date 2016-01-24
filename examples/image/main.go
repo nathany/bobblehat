4f28fb92fada215287c6c2888288d90d1da73194
Nathan Youngman <git@nathany.com>
2016-01-24 11:37:56 -0700

    image panning: handle ctrl-c

--- examples/image/main.go
+++ examples/image/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	_ "image/png"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gophergala2016/bobblehat/sense/screen"
@@ -21,18 +23,22 @@ func main() {
 	tx, err := texture.Load(os.Args[1])
 	fatalIfError(err)
 
-	// display top corner of image and wait a bit
-	texture.Blit(fb.Texture, 0, 0, tx, 0, 0, 8, 8)
-	screen.Draw(fb)
-	time.Sleep(500 * time.Millisecond)
+	// setup channel to handle Ctrl-C events
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGUSR1, syscall.SIGINT, syscall.SIGTERM)
 
 	// pan around the image
-	state := 1
+	state := 0
 	var xo, yo int
+
+loop:
 	for {
 		sleep := 100
 
 		switch state {
+		case 0: // nothing
+			state = 1
+			sleep = 500
 		case 1: // down
 			yo++
 			if yo == tx.Height()-8 {
@@ -61,8 +67,18 @@ func main() {
 
 		texture.Blit(fb.Texture, 0, 0, tx, xo, yo, 8, 8)
 		screen.Draw(fb)
+
+		// check if Ctrl-C was pressed
+		select {
+		case <-quit:
+			break loop
+		default:
+		}
+
 		time.Sleep(time.Duration(sleep) * time.Millisecond)
 	}
+
+	screen.Clear()
 }
 
 func fatalIfError(err error) {
