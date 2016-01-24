49f10d45b8014e88906d6bd7b289efd3b7577661
Nathan Youngman <git@nathany.com>
2016-01-23 22:08:41 -0700

    file for image demo

--- examples/image/main.go
+++ examples/image/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "image/png"
+	"os"
 	"time"
 
 	"github.com/gophergala2016/bobblehat/sense/screen"
@@ -11,9 +12,13 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("specify a png image to load.")
+	}
+
 	fb := screen.NewFrameBuffer()
 
-	tx, err := texture.Load("gopher16x16.png")
+	tx, err := texture.Load(os.Args[1])
 	fatalIfError(err)
 
 	// display top corner of image and wait a bit
@@ -25,31 +30,38 @@ func main() {
 	state := 1
 	var xo, yo int
 	for {
-		texture.Blit(fb.Texture, 0, 0, tx, xo, yo, 8, 8)
-		screen.Draw(fb)
+		sleep := 100
+
 		switch state {
 		case 1: // down
 			yo++
 			if yo == tx.Height()-8 {
 				state = 2
+				sleep = 500
 			}
 		case 2: // right
 			xo++
 			if xo == tx.Width()-8 {
 				state = 3
+				sleep = 500
 			}
 		case 3: // up
 			yo--
 			if yo == 0 {
 				state = 4
+				sleep = 500
 			}
 		case 4: // left
 			xo--
 			if xo == 0 {
 				state = 1
+				sleep = 500
 			}
 		}
-		time.Sleep(100 * time.Millisecond)
+
+		texture.Blit(fb.Texture, 0, 0, tx, xo, yo, 8, 8)
+		screen.Draw(fb)
+		time.Sleep(time.Duration(sleep) * time.Millisecond)
 	}
 }
 
