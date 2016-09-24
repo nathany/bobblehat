fca4e2c92f3f03912e542d3bff6b0c198daa74b8
Peter Hellberg <peter@c7.se>
2016-09-24 18:57:47 +0200

    Minimal evdev implementation (directions + Enter)
    Issue #13

--- sense/stick/stick.go
+++ sense/stick/stick.go
@@ -1,73 +1,115 @@
 package stick
 
 import (
-	"errors"
-	"fmt"
-	"io/ioutil"
 	"os"
-	"path/filepath"
 	"strings"
+	"syscall"
+	"unsafe"
 )
 
-// Joystick events
-var (
-	Right = 0
-	Left  = 1
-	Up    = 2
-	Down  = 3
-	Enter = 4
+const (
+	Enter = 28
+	Up    = 103
+	Left  = 105
+	Right = 106
+	Down  = 108
 )
 
-// the joy stick
-var joyStickDevice string
+type Device struct {
+	f      *os.File
+	Events chan Event
+}
 
-func init() {
+func Open(name string) (*Device, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+
+	d := &Device{f, make(chan Event, 4)}
+
+	go d.pollEvents()
+
+	return d, nil
+}
+
+func (d *Device) Close() error {
 	var err error
-	joyStickDevice, err = getDevice("Raspberry Pi Sense HAT Joystick")
-	if err != nil {
-		panic(err)
+
+	if d.f != nil {
+		err = d.f.Close()
+		d.f = nil
 	}
+
+	return err
 }
 
-// ReadEvent from the joystick.
-func ReadEvent() int {
-	file, err := os.Open(joyStickDevice)
-	if err != nil {
-		panic(err)
-	}
+func (d *Device) Name() string {
+	var str [256]byte
 
-	data := make([]byte, 100)
-	count, err := file.Read(data)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("read %d bytes, %q\n", count, data[:count])
+	ioctl(d.f.Fd(), _EVIOCGNAME(256), uintptr(unsafe.Pointer(&str[0])))
 
-	return count
+	return strings.TrimRight(string(str[:]), "\x00")
 }
 
-func getDevice(name string) (string, error) {
-	matches, err := filepath.Glob("/sys/class/input/event*")
-	if err != nil {
-		return "", err
-	}
+func (d *Device) pollEvents() {
+	defer close(d.Events)
 
-	for _, dir := range matches {
-		b, err := ioutil.ReadFile(filepath.Join(dir, "device/name"))
+	var e Event
+
+	size := int(unsafe.Sizeof(e))
+	buf := make([]byte, size*2)
+
+	for {
+		n, err := d.f.Read(buf)
 		if err != nil {
-			continue
+			return
 		}
-		fbName := strings.TrimSpace(string(b))
-		if fbName == name {
-			dev := filepath.Join("/dev/input", filepath.Base(dir))
 
-			return dev, nil
+		events := (*(*[1<<27 - 1]Event)(unsafe.Pointer(&buf[0])))[:n/size]
+
+		for i := range events {
+			if e := events[i]; e.Type == 0x01 && e.Value != 0 {
+				d.Events <- e
+			}
 		}
 	}
-	return "", errStickDeviceNotFound
 }
 
-// errors
-var (
-	errStickDeviceNotFound = errors.New("stick device not found")
+// Event represents a generic input event.
+type Event struct {
+	Time  syscall.Timeval
+	Type  uint16
+	Code  uint16
+	Value int32
+}
+
+func ioctl(fd, name, v uintptr) error {
+	_, _, errno := syscall.RawSyscall(syscall.SYS_IOCTL, fd, name, v)
+	if errno == 0 {
+		return nil
+	}
+
+	return errno
+}
+
+func _EVIOCGNAME(len int) uintptr {
+	return _IOC(_IOC_READ, 'E', 0x06, len)
+}
+
+const (
+	_IOC_WRITE     = 0x1
+	_IOC_READ      = 0x2
+	_IOC_NRBITS    = 8
+	_IOC_TYPEBITS  = 8
+	_IOC_SIZEBITS  = 14
+	_IOC_NRSHIFT   = 0
+	_IOC_TYPESHIFT = _IOC_NRSHIFT + _IOC_NRBITS
+	_IOC_SIZESHIFT = _IOC_TYPESHIFT + _IOC_TYPEBITS
+	_IOC_DIRSHIFT  = _IOC_SIZESHIFT + _IOC_SIZEBITS
 )
+
+func _IOC(dir, t, nr, size int) uintptr {
+	return uintptr((dir << _IOC_DIRSHIFT) | (t << _IOC_TYPESHIFT) |
+		(nr << _IOC_NRSHIFT) | (size << _IOC_SIZESHIFT))
+}
---
+++ sense/stick/stick_test.go
@@ -0,0 +1,45 @@
+package stick
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestStickWithEmptyDeviceFile(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "example")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name())
+
+	for i := 0; i < 100; i++ {
+		tmpfile.Write([]byte{0x30, 0xab, 0xe6, 0x57, 0xc0, 0x80, 0x8, 0x0, 0x1, 0x0, 0x1c, 0x0, 0x1, 0x0, 0x0, 0x0, 0x30, 0xab, 0xe6, 0x57, 0xc0, 0x80, 0x8, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0})
+	}
+	tmpfile.Sync()
+
+	input, err := Open(tmpfile.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer input.Close()
+
+	if got, want := input.Name(), ""; got != want {
+		t.Fatalf("input.Name() = %q, want %q", got, want)
+	}
+
+	timeout := time.After(5 * time.Second)
+
+	for {
+		select {
+		case e := <-input.Events:
+			if e.Type == 1 && e.Code == Enter {
+				t.Logf("Enter was pressed!")
+				return
+			}
+		case <-timeout:
+			t.Fatal("could not read any events")
+		}
+	}
+}
