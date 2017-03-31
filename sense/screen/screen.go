630075556c01f79723abd3bcec22dbdc586b37df
Peter Hellberg <peter@c7.se>
2017-03-31 22:41:30 +0200

    Support drawing image.Image
    *FrameBuffer now implements the draw.Image and image.Image interfaces
    
    Change NewFrameBuffer to use functional options
    
    Do not panic if unable to find RPi-Sense FB

--- sense/screen/screen.go
+++ sense/screen/screen.go
@@ -4,11 +4,14 @@ package screen
 import (
 	"encoding/binary"
 	"errors"
+	"image"
+	"image/color"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 
+	rgb565color "github.com/nathany/bobblehat/sense/screen/color"
 	"github.com/nathany/bobblehat/sense/screen/texture"
 )
 
@@ -18,9 +21,81 @@ type FrameBuffer struct {
 }
 
 // NewFrameBuffer creates a back buffer for the screen.
-func NewFrameBuffer() *FrameBuffer {
-	return &FrameBuffer{
+func NewFrameBuffer(options ...func(*FrameBuffer)) *FrameBuffer {
+	fb := &FrameBuffer{
 		Texture: texture.New(8, 8),
+	}
+
+	for _, o := range options {
+		o(fb)
+	}
+
+	return fb
+}
+
+// ColorModel of the frame buffer
+func (fb *FrameBuffer) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+// Bounds of the frame buffer (it is 8x8)
+func (fb *FrameBuffer) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 8, 8)
+}
+
+// At returns the color of the LED at x,y
+func (fb *FrameBuffer) At(x, y int) color.Color {
+	if x < 0 || y < 0 || x > 7 || y > 7 {
+		return color.RGBA{}
+	}
+
+	sc := uint16(fb.GetPixel(x, y))
+
+	r := (sc & 0xF800) >> 8
+	g := (sc & 0x07E0) >> 3
+	b := (sc & 0x001F) << 3
+
+	return color.RGBA{
+		R: uint8(r),
+		G: uint8(g),
+		B: uint8(b),
+		A: 0xff,
+	}
+}
+
+// Set the color of the LED at x,y
+func (fb *FrameBuffer) Set(x, y int, c color.Color) {
+	if x < 0 || y < 0 || x > 7 || y > 7 {
+		return
+	}
+
+	r, g, b, _ := c.RGBA()
+
+	if fb.Texture == nil {
+		fb.Texture = texture.New(8, 8)
+	}
+
+	fb.Texture.SetPixel(x, y, rgb565color.New(uint8(r>>8), uint8(g>>8), uint8(b>>8)))
+}
+
+// SetImage sets the frame buffer to the provided image
+func (fb *FrameBuffer) SetImage(m image.Image) {
+	for y := 0; y < 8; y++ {
+		for x := 0; x < 8; x++ {
+			r, g, b, _ := m.At(x, y).RGBA()
+			fb.Texture.SetPixel(x, y, rgb565color.New(uint8(r>>8), uint8(g>>8), uint8(b>>8)))
+		}
+	}
+}
+
+// DrawImage draws an image to the LED matrix screen.
+func DrawImage(m image.Image) error {
+	return Draw(NewFrameBuffer(withImage(m)))
+}
+
+func withImage(m image.Image) func(*FrameBuffer) {
+	return func(fb *FrameBuffer) {
+		fb.SetImage(m)
 	}
 }
 
@@ -41,9 +116,10 @@ var displayDevice string
 
 func init() {
 	var err error
+
 	displayDevice, err = getDevice("RPi-Sense FB")
 	if err != nil {
-		panic(err)
+		os.Stdout.WriteString(err.Error() + "\n")
 	}
 }
 
